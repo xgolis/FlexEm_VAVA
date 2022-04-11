99069df82f6abdd36c1481c623feb3d29f5f07e8
xgolis <xgolis@stuba.sk>
2022-04-11 21:34:31 +0200

    vytvorenie_packagov

--- src/main/java/com/example/flexem/controller/HelloController.java
+++ src/main/java/com/example/flexem/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.flexem;
+package com.example.flexem.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.flexem {
 
     opens com.example.flexem to javafx.fxml;
     exports com.example.flexem;
+    exports com.example.flexem.controller;
+    opens com.example.flexem.controller to javafx.fxml;
 }
