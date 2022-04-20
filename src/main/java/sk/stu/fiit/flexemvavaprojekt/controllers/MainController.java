e5f02b2fd05bd167f4b8652b81e93fa260f9ba86
jozefvlcek <xvlcekj@stuba.sk>
2022-04-20 18:23:51 +0200

    diakritika v prekladoch

--- src/main/java/sk/stu/fiit/flexemvavaprojekt/controllers/MainController.java
+++ src/main/java/sk/stu/fiit/flexemvavaprojekt/controllers/MainController.java
@@ -1,7 +1,5 @@
 package sk.stu.fiit.flexemvavaprojekt.controllers;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import sk.stu.fiit.flexemvavaprojekt.models.Jazyk;
