1ae7ff5505cfeb8d9e19493d1d0a26d078c7eafe
xgolis <xgolis@stuba.sk>
2022-04-22 15:06:50 +0200

    uprava pouzivatela

--- src/main/java/sk/stu/fiit/flexemvavaprojekt/models/Cvicenec.java
+++ src/main/java/sk/stu/fiit/flexemvavaprojekt/models/Cvicenec.java
@@ -2,8 +2,28 @@ package sk.stu.fiit.flexemvavaprojekt.models;
 
 public class Cvicenec extends Pouzivatel{
 
+    private int skupinovyPlanId;
+    private int individualnyPlanId;
 
-    public Cvicenec(int id, String meno, String priezvisko, String email, String telefon, String heslo, int skupinovyPlanId) {
-        super(id, meno, priezvisko, email, telefon, heslo , skupinovyPlanId);
+    public Cvicenec(int id, String meno, String priezvisko, String email, String telefon, String heslo, int skupinovyPlanId, int individualnyPlanId) {
+        super(id, meno, priezvisko, email, telefon, heslo);
+        this.skupinovyPlanId = skupinovyPlanId;
+        this.individualnyPlanId = individualnyPlanId;
+    }
+
+    public int getSkupinovyPlanId() {
+        return skupinovyPlanId;
+    }
+
+    public int getIndividualnyPlanId() {
+        return individualnyPlanId;
+    }
+
+    public void setSkupinovyPlanId(int skupinovyPlanId) {
+        this.skupinovyPlanId = skupinovyPlanId;
+    }
+
+    public void setIndividualnyPlanId(int individualnyPlanId) {
+        this.individualnyPlanId = individualnyPlanId;
     }
 }
