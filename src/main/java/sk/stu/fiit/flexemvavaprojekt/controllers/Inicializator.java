1fefd53f99341ae1bb2b2c93ed165df2ef5171ff
xgolis <xgolis@stuba.sk>
2022-04-24 15:28:45 +0200

    tabulky

--- src/main/java/sk/stu/fiit/flexemvavaprojekt/controllers/Inicializator.java
+++ src/main/java/sk/stu/fiit/flexemvavaprojekt/controllers/Inicializator.java
@@ -4,13 +4,17 @@ import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import org.w3c.dom.ls.LSOutput;
 import sk.stu.fiit.flexemvavaprojekt.db.DbConnector;
 import sk.stu.fiit.flexemvavaprojekt.models.Jazyk;
 import sk.stu.fiit.flexemvavaprojekt.models.Pouzivatel;
+import sk.stu.fiit.flexemvavaprojekt.models.Recenzia;
 import sk.stu.fiit.flexemvavaprojekt.models.Trener;
+
+import java.util.ArrayList;
 
 public class Inicializator {
 
@@ -31,34 +35,44 @@ public class Inicializator {
     }
 
     public static void inicializujTrenerovChoiceBox(ChoiceBox<String> choiceBox){
-        int i = 2;
-        Trener trener = DbConnector.getInstance().getTrener(i);
-        choiceBox.setValue(trener.getMeno() + " " + trener.getPriezvisko());
-        do {
+        ArrayList<Pouzivatel> treners = DbConnector.getInstance().getAllTreners();
+        System.out.println(treners);
+        choiceBox.setValue(treners.get(0).getMeno() + " " + treners.get(0).getPriezvisko());
+
+        for (Pouzivatel trener: treners){
             choiceBox.getItems().add(trener.getMeno() + " " + trener.getPriezvisko());
-            i++;
-            trener = DbConnector.getInstance().getTrener(i);
-        } while (trener != null);
+        }
     }
 
     public static void inicializujTabulkuTreningovyPlanCvicenci(TableView<Pouzivatel> tabulka,
-                                                                TableColumn<Pouzivatel, String> indivTMenoCollum,
-                                                                TableColumn<Pouzivatel, String> indivTPriezviskoCollum){
+                                                                TableColumn<Pouzivatel, String> indivTMenoColumn,
+                                                                TableColumn<Pouzivatel, String> indivTPriezviskoColumn){
 
-        int i = 3;
-        Pouzivatel cvicenec = DbConnector.getInstance().getCvicenec(i);
-        ObservableList<Pouzivatel> lanes= FXCollections.observableArrayList();
-        do {
-            lanes.add(cvicenec);
-            System.out.println(cvicenec);
-            i++;
-            cvicenec = DbConnector.getInstance().getCvicenec(i);
-        } while (cvicenec != null);
+        ArrayList<Pouzivatel> cvicenec = DbConnector.getInstance().getAllCvicenecs();
+        ObservableList<Pouzivatel> lanes = FXCollections.observableArrayList();
+        lanes.addAll(cvicenec);
 
-        indivTMenoCollum.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getPriezvisko()));
-        indivTPriezviskoCollum.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getMeno()));
+        indivTPriezviskoColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getPriezvisko()));
+        indivTMenoColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getMeno()));
         tabulka.setItems(lanes);
     }
+
+//    public static void inicializujTabulkuRecenzii(TableView<Recenzia> tabulka,
+//                                                  TableColumn<Recenzia, String> menoColumn,
+//                                                  TableColumn<Recenzia, String> priezviskoColumn,
+//                                                  TableColumn<Recenzia, String> sportColumn,
+//                                                  TableColumn<Recenzia, String> hvColumn){
+//
+//        ArrayList<Recenzia> recenzie = DbConnector.getInstance().getAllRecenzias();
+//        ObservableList<Recenzia> lanes = FXCollections.observableArrayList();
+//        lanes.addAll(recenzie);
+//
+//        menoColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getPriezvisko()));
+//        priezviskoColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue().getMeno()));
+//
+//        tabulka.setItems(lanes);
+//
+//    }
 }
 
 
