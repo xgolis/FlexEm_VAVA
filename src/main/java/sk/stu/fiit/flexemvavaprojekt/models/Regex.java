3f2ce92e510fc3b7f3d6cff3b1ade74ed02421f0
xgolis <xgolis@stuba.sk>
2022-04-24 23:19:25 +0200

    neviem ani čo

--- src/main/java/sk/stu/fiit/flexemvavaprojekt/models/Regex.java
+++ src/main/java/sk/stu/fiit/flexemvavaprojekt/models/Regex.java
@@ -14,11 +14,11 @@ public class Regex {
 
     public static ArrayList<String> vytvorMozneRegexy(String text){
         ArrayList<String> list = new ArrayList<>();
-        list.add(".*" + text + ".*");
+        list.add(text + ".*");
         int dlzka = text.length();
 
         if (text.length() == 1) {
-            list.add(".*" + text + ".*");
+            list.add(text + ".*");
         }
         else {
 
@@ -31,7 +31,7 @@ public class Regex {
                     temp = temp.substring(0, i - 1) + '.' + temp.substring(i);
                 }
 
-                list.add(".*" + temp + ".*");
+                list.add(temp + ".*");
             }
 
         }
