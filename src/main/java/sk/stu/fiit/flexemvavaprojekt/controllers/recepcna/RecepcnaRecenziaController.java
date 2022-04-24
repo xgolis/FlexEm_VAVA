fd0e6b8682d810d86806778f2fa4afdeacf84f4b
MarošHnat <makohnat@gmail.com>
2022-04-24 12:56:00 +0200

    Error

--- src/main/java/sk/stu/fiit/flexemvavaprojekt/controllers/recepcna/RecepcnaRecenziaController.java
+++ src/main/java/sk/stu/fiit/flexemvavaprojekt/controllers/recepcna/RecepcnaRecenziaController.java
@@ -5,15 +5,29 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import sk.stu.fiit.flexemvavaprojekt.models.InputValidation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import sk.stu.fiit.flexemvavaprojekt.models.Recenzia;
 import sk.stu.fiit.flexemvavaprojekt.router.Router;
 import sk.stu.fiit.flexemvavaprojekt.router.RouterEnum;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RecepcnaRecenziaController implements Initializable {
+
+    private Recenzia review;
 
     @FXML
     private TextField recenziaRHviezdyField;
@@ -31,6 +45,10 @@ public class RecepcnaRecenziaController implements Initializable {
     private TextField recenziaRSportFIeld;
     @FXML
     private Label actionLabel;
+
+    public RecepcnaRecenziaController(Recenzia review) {
+        this.review = review;
+    }
 
     @FXML
     protected void odhlasenie() {
@@ -108,7 +126,44 @@ public class RecepcnaRecenziaController implements Initializable {
 
     @FXML
     protected void sendReview(){
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.newDocument();
 
+            Element recenzia = doc.createElement("Recenzia");
+            doc.appendChild(recenzia);
+
+            Element miestnost = doc.createElement("Miestnost");
+            miestnost.appendChild(doc.createTextNode(review.getMiestnost()));
+            recenzia.appendChild(miestnost);
+
+            Element sport = doc.createElement("Sport");
+            sport.appendChild(doc.createTextNode(review.getSport()));
+            recenzia.appendChild(sport);
+
+            Element pocetHviezd = doc.createElement("Hodnotenie");
+            pocetHviezd.appendChild(doc.createTextNode(String.valueOf(review.getPocetHviezd())));
+            recenzia.appendChild(pocetHviezd);
+
+            Element popis = doc.createElement("Popis hodnotenia");
+            popis.appendChild(doc.createTextNode(review.getPopis()));
+            recenzia.appendChild(popis);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+
+            StreamResult result = new StreamResult(new File("recenzia.xml"));
+            transformer.transform(source, result);
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
     }
 
 }
